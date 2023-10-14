9db136a960d5b3a57ad341555d0539f0d398c3e5
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-14 14:25:39 -0400

    by lecture 29

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Text('Hello World!!'),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+               colors:
+            )
+          ),
+          child: Center(
+            child: Text('Hello World!!'),
+          ),
         ),
       ),
     ),
