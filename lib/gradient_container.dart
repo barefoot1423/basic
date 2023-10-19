580fa14a2a04d08f3c3fd757ce08fc2ee116a07f
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-18 23:02:03 -0400

    by lecture 46

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,6 +9,8 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  void rollDice() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,7 +22,22 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset('assets/images/dice-2.png', width: 200),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 200),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                onPressed: rollDice,
+                style: TextButton.styleFrom(
+                    // padding: EdgeInsets.only(top: 20),
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(fontSize: 28)),
+                child: const Text('Roll Dice'))
+          ],
+        ),
       ),
     );
   }
