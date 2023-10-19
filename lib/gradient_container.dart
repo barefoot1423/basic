8942a7decb8309bbaa6a97c57e2d153e0dba47a0
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-18 22:45:39 -0400

    by lecture 44

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -19,7 +19,9 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText('Hello World!')),
+      child: Center(
+        child: Image.asset('assets/images/dice-2.png', width: 200),
+      ),
     );
   }
 }
